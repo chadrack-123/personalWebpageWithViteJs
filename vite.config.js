4f5e01a6c7b8466e955d1a7c4ea34f923af6f012
chadrack-123 <chadrackmulamba2@gmail.com>
2024-06-07 10:59:16 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
-  plugins: [
-    handlebars({
-      context: data
-    }),
-  ],
+  base : '/responsive_webpages/',   // <-- add this
+    plugins: [handlebars({
+        context : {
+            /*
+            previous setup here
+            */
+            data   // add this
+        }
+    })],
 };
