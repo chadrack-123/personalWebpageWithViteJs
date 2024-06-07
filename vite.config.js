74d43e16061df05815a4fbceeb1bce91515cb869
chadrack-123 <chadrackmulamba2@gmail.com>
2024-06-07 11:09:32 +0200

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
 import data from './data.json';
 
-export default {
-  base : '/responsive_webpages/',   // <-- add this
+export default defineConfig({
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+            }
+        },
+    },
     plugins: [handlebars({
-        context : {
-            /*
-            previous setup here
-            */
-            data   // add this
-        }
-    })],
-};
+      context : {
+        data
+      }
+  })]
+    
+});
